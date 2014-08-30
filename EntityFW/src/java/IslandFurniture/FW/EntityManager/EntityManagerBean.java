a7a2bfa669884bc26b912864a9e47d38c52bb9ec
Benjamin Lim <mail@limbenjamin.com>
2014-08-31 00:44:54 +0800

    Added entitymanager in EntityFW, change to jdbc/furniture

---
+++ EntityFW/src/java/IslandFurniture/FW/EntityManager/EntityManagerBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IslandFurniture.FW.EntityManager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Benjamin
+ */
+@Stateless
+@LocalBean
+public class EntityManagerBean {
+
+    @PersistenceContext
+    EntityManager em;
+    
+    public EntityManagerBean(){
+        
+    }
+}
