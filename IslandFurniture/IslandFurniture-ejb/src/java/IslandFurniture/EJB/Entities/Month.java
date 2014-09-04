03b8366fa02f8f0a412f72eece859da9a87f5bf6
James <James@PowerDog64Bit>
2014-09-04 21:50:35 +0800

    Working of production planning Manufacturing

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/Month.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/Month.java
@@ -3,13 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package IslandFurniture.EJB.Entities;
 
 /**
  *
  * @author Chen Tong <chentong@nus.edu.sg>
+ * I have added a helper function to translate enum to month. See
+ * Helper.TranslateMonth
  */
 public enum Month {
-    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+
+    JAN(1), FEB(2), MAR(3), APR(4), MAY(5), JUN(6), JUL(7), AUG(8), SEP(9), OCT(10), NOV(11), DEC(12);
+
+    public int value;
+
+    private Month(int value) {
+        this.value = value;
+
+    }
+
 }
--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/WeeklyProductionPlan.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/WeeklyProductionPlan.java
@@ -28,14 +28,15 @@ public class WeeklyProductionPlan implements Serializable {
     private MonthlyProductionPlan monthlyProductionPlan;
     @OneToOne
     private ProductionOrder productionOrder;
+    
+    private int WeekNo;
+    
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+
 
     public MonthlyProductionPlan getMonthlyProductionPlan() {
         return monthlyProductionPlan;
@@ -76,6 +77,14 @@ public class WeeklyProductionPlan implements Serializable {
     @Override
     public String toString() {
         return "FW.IslandFurniture.Entities.MANUFACTURING.WeeklyProductionPlan[ id=" + id + " ]";
+    }
+
+    public int getWeekNo() {
+        return WeekNo;
+    }
+
+    public void setWeekNo(int WeekNo) {
+        this.WeekNo = WeekNo;
     }
     
 }
