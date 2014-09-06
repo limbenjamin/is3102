1d4c8bf343947e0837c074ff8b84a0f13bb5e70f
Benjamin Lim <mail@limbenjamin.com>
2014-09-06 15:16:53 +0800

    Added missing WEB-INF folder and files, removed sun-resources, changed announcement/accesscontrol entity to session bean

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/CommonInfrastructure/ManageAnnouncementsBean.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/CommonInfrastructure/ManageAnnouncementsBean.java
@@ -6,54 +6,17 @@
 
 package IslandFurniture.EJB.CommonInfrastructure;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
 
 /**
  *
  * @author Benjamin
  */
-@Entity
-public class ManageAnnouncementsBean implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+@Stateless
+@LocalBean
+public class ManageAnnouncementsBean {
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof ManageAnnouncementsBean)) {
-            return false;
-        }
-        ManageAnnouncementsBean other = (ManageAnnouncementsBean) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "IslandFurniture.CommonInfrastructure.ManageAnnouncements[ id=" + id + " ]";
-    }
-    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
 }
