6291fc665120a166800cccb54909ee6f5c37998c
Benjamin Lim <mail@limbenjamin.com>
2014-09-08 19:03:54 +0800

    moved login controller to loginMangedBean, fixing the dashboard not refreshing bug

---
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/CommonInfrastructure/LoginManagedBean.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IslandFurniture.WAR.CommonInfrastructure;
+
+/**
+ *
+ * @author Benjamin
+ */
+
+
+import IslandFurniture.EJB.CommonInfrastructure.*;
+import IslandFurniture.EJB.Entities.*;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+
+@Named
+@SessionScoped
+public class LoginManagedBean implements Serializable {
+    private static final long serialVersionUID = 5443351151396868724L;
+    private String absolutepath = "http://localhost:8080/IslandFurniture-erp-war/";
+    private Staff staff;
+    private String username = null;
+    private String password = null;
+    
+    @EJB
+    private ManageAuthenticationBean authBean;  
+    
+    public LoginManagedBean() {
+    }
+    
+    public String login() {
+        boolean result = authBean.authenticate(username, password);
+        if (result) {
+            HttpSession session = Util.getSession();
+            session.setAttribute("username", username);
+            return "dash";
+        } else {
+ 
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                    "Invalid Login!",
+                    "Please Try Again!"));
+ 
+            // invalidate session, and redirect to other pages
+ 
+            //message = "Invalid Login. Please Try Again!";
+            return "login";
+        }
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ManageAuthenticationBean getAuthBean() {
+        return authBean;
+    }
+
+    public void setAuthBean(ManageAuthenticationBean authBean) {
+        this.authBean = authBean;
+    }
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getAbsolutepath() {
+        return absolutepath;
+    }
+
+    public void setAbsolutepath(String absolutepath) {
+        this.absolutepath = absolutepath;
+    }
+    
+    
+
+}
