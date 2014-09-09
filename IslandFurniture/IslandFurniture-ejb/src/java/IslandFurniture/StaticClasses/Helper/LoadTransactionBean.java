cfa0264252bf9d3be5afd7f90844177e835296b9
Chen Tong <chentong@nus.edu.sg>
2014-09-09 13:05:13 +0800

    Split the Data Loading Bean into 2 different ones. LoadOrgEntitiesBean only loads org entities while LoadTransactionBean loads many many transactions. Run chentongclient to load either, with the choice indicated within the command line prompt.

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/StaticClasses/Helper/LoadTransactionBean.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/StaticClasses/Helper/LoadTransactionBean.java
@@ -3,25 +3,95 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package IslandFurniture.StaticClasses.Helper;
 
+import IslandFurniture.EJB.Entities.FurnitureModel;
 import IslandFurniture.EJB.Entities.FurnitureTransaction;
+import IslandFurniture.EJB.Entities.FurnitureTransactionDetail;
 import IslandFurniture.EJB.Entities.Store;
+import IslandFurniture.EJB.RemoteInterfaces.LoadTransactionBeanRemote;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+import java.util.TimeZone;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.REQUIRED;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author Chen Tong <chentong@nus.edu.sg>
  */
 @Stateless
-public class LoadTransactionBean implements LoadTransactionBeanLocal {
+public class LoadTransactionBean implements LoadTransactionBeanRemote {
 
-    @Override
-    public FurnitureTransaction addFurnitureTransaction(Store store) {
-        return null;
+    @PersistenceContext(unitName = "IslandFurniture")
+    private EntityManager em;
+
+    private FurnitureTransaction addFurnitureTransaction(Store store, List<FurnitureTransactionDetail> fTransDetails, Calendar transTime) {
+        FurnitureTransaction fTrans = new FurnitureTransaction();
+        fTrans.setStore(store);
+        fTrans.setTransTime(transTime);
+        for (FurnitureTransactionDetail eachDetail : fTransDetails) {
+            eachDetail.setFurnitureTransaction(fTrans);
+        }
+        fTrans.setFurnitureTransactionDetails(fTransDetails);
+
+        em.persist(fTrans);
+
+        System.out.println(fTransDetails.get(0).getFurnitureTransaction());
+
+        return fTrans;
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    private FurnitureTransactionDetail addFurnitureTransactionDetail(FurnitureModel furniture, int qty) {
+        FurnitureTransactionDetail fTransDetail = new FurnitureTransactionDetail();
+        fTransDetail.setFurnitureModel(furniture);
+        fTransDetail.setQty(qty);
+
+        return fTransDetail;
+    }
+
+    @Override
+    @TransactionAttribute(REQUIRED)
+    public boolean loadSampleData() {
+
+        FurnitureTransaction fTrans;
+        List<FurnitureTransactionDetail> fTransDetails = new ArrayList();
+
+        try {
+            // Add Transactions for stores
+            Calendar cal;
+            Random rand = new Random();
+            List<Store> stores = (List<Store>) em.createNamedQuery("getAllStores").getResultList();
+            List<FurnitureModel> furnitureModels = (List<FurnitureModel>) em.createNamedQuery("getAllFurnitureModels").getResultList();
+
+            for (int i = 0; i < 800; i++) {
+                for (Store eachStore : stores) {
+                    fTransDetails.clear();
+                    for (FurnitureModel fm : furnitureModels) {
+                        if (rand.nextBoolean()) {
+                            fTransDetails.add(this.addFurnitureTransactionDetail(fm, rand.nextInt(50) + 1));
+                        }
+                    }
+
+                    if (!fTransDetails.isEmpty()) {
+                        cal = Calendar.getInstance(TimeZone.getTimeZone(eachStore.getCountry().getTimeZoneID()));
+                        cal.set(rand.nextInt(28) + 1, rand.nextInt(12) + 1, rand.nextInt(2) + 2013, rand.nextInt(13) + 10, rand.nextInt(60), rand.nextInt(60));
+                        fTrans = this.addFurnitureTransaction(eachStore, fTransDetails, cal);
+                    }
+                }
+            }
+
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            
+            return false;
+        }
+
+    }
 }
--- chentongclient/src/java/chentongclient/Main.java
+++ chentongclient/src/java/chentongclient/Main.java
@@ -6,16 +6,21 @@
 package chentongclient;
 
 import IslandFurniture.EJB.RemoteInterfaces.LoadOrgEntitiesBeanRemote;
+import IslandFurniture.EJB.RemoteInterfaces.LoadTransactionBeanRemote;
+import java.util.Scanner;
 import javax.ejb.EJB;
 
 /**
- * IMPORTANT!
- * Data Loading client for sales forecasting. around 16k lines of Transaction
- * Detail data loaded each run. Please do not run too many times.
+ * IMPORTANT! Data Loading client for organisation data and transaction data. 
+ * Around 16k lines of Transaction Detail data loaded when loading transaction 
+ * data. Please do not run too many times.
  *
  * @author Chen Tong <chentong@nus.edu.sg>
  */
 public class Main {
+
+    @EJB
+    private static LoadTransactionBeanRemote loadTransactionBean;
 
     @EJB
     private static LoadOrgEntitiesBeanRemote loadOrgEntitiesBean;
@@ -24,13 +29,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Load Organisation Entities (Store, Manufacturing Facility, Country Office)
+        Scanner sc = new Scanner(System.in);
 
-        if (loadOrgEntitiesBean.loadSampleData()) {
-            System.out.println("Data loaded successfully");
+        // Load Organisation Entities (Store, Manufacturing Facility, Country Office) & FurnitureModels
+        System.out.print("Load Organisation & Furniture Data? (y/n):");
+        if (sc.nextLine().equalsIgnoreCase("y")) {
+            if (loadOrgEntitiesBean.loadSampleData()) {
+                System.out.println("Organisation Data & Furniture data loaded successfully!");
+            } else {
+                System.out.println("Failed to load Organisation and furniture data. Check for existing data and/or recreate islandFurniture database");
+            }
         }
-        else{
-            System.out.println("Failed to load data. Check for existing data and/or recreate islandFurniture database");
+
+        // Load Transaction Data (Furniture Only for now)
+        System.out.print("Load Transaction Data? (y/n):");
+        if (sc.nextLine().equalsIgnoreCase("y")) {
+            if (loadTransactionBean.loadSampleData()) {
+                System.out.println("Transaction data loaded successfully!");
+            } else {
+                System.out.println("Failed to load Transaction data. Check for existing data and/or recreate islandFurniture database");
+            }
         }
     }
 
