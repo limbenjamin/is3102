b5f1a6a2ee42c6c75df29240f54798748cf01912
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-09-17 08:00:54 +0800

    CRUD for StorageArea and StorageBin entities at EJB, ManagedBean and ViewLayers

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/StorageBin.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Entities/StorageBin.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package IslandFurniture.EJB.Entities;
 
 import java.io.Serializable;
@@ -16,52 +15,20 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PostPersist;
 
- /**
- *      @author Kamilul Ashraf
- *
- *      List of Attributes for Storage Location
- *
- * 1.   Plant Number - Integer
- *      Plant/Store Number (eg. 1, 2, 3)
- * 
- * 2.   Storage Area Number - Integer
- *      Area Number (eg. 1, 2.. )
- * 
- * 3.   Storage Area Name - String
- *      Area Name (eg. Staging Area, Receiving Area)
- * 
- * 4.   Storage ID - String
- *      Storage ID (eg. A12, B55, C42.. )
- * 
- * 5.   Storage Type - String
- *      Type of Storage (eg. Public or Private)
- * 
- * 6.   Storage Description - String
- *      Description of Storage Space (eg. Usually store SKU 1234)
- *	
- */
-
 @Entity
 public class StorageBin implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
-    @OneToMany(mappedBy="location")
+    @OneToMany(mappedBy = "location")
     private List<StockUnit> stockUnits;
-    
     @ManyToOne
     private StorageArea storageArea;
-    
-    private Integer plantNumber;
-    private Integer storageAreaNumber;
-    private String storageAreaName;
-    private String storageID;
-    private String storageType;
-    private String storageDescription;
-    
-        public Long getId() {
+    private String name;
+
+    public Long getId() {
         return id;
     }
 
@@ -77,57 +44,21 @@ public class StorageBin implements Serializable {
         this.stockUnits = stockUnits;
     }
 
-    public Integer getPlantNumber() {
-        return plantNumber;
+    public StorageArea getStorageArea() {
+        return storageArea;
     }
 
-    public void setPlantNumber(Integer plantNumber) {
-        this.plantNumber = plantNumber;
+    public void setStorageArea(StorageArea storageArea) {
+        this.storageArea = storageArea;
     }
 
-    public Integer getStorageAreaNumber() {
-        return storageAreaNumber;
+    public String getName() {
+        return name;
     }
 
-    public void setStorageAreaNumber(Integer storageAreaNumber) {
-        this.storageAreaNumber = storageAreaNumber;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public String getStorageAreaName() {
-        return storageAreaName;
-    }
-
-    public void setStorageAreaName(String storageAreaName) {
-        this.storageAreaName = storageAreaName;
-    }
-
-    public String getStorageID() {
-        return storageID;
-    }
-
-    public void setStorageID(String storageID) {
-        this.storageID = storageID;
-    }
-
-    public String getStorageType() {
-        return storageType;
-    }
-
-    public void setStorageType(String storageType) {
-        this.storageType = storageType;
-    }
-
-    public String getStorageDescription() {
-        return storageDescription;
-    }
-
-    public void setStorageDescription(String storageDescription) {
-        this.storageDescription = storageDescription;
-    }
-
-
-
-
 
     @Override
     public int hashCode() {
@@ -151,11 +82,10 @@ public class StorageBin implements Serializable {
 
     @Override
     public String toString() {
-        return "StorageLocation[ id=" + id + " ]";
+        return "StorageBin[ id=" + id + " ]";
     }
-    
-    // Entity Callbacks
 
+    // Entity Callbacks
     @PostPersist
     public void postPersist() {
         System.out.println("Successfully persisted " + this);
