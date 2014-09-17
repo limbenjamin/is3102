9d2fade73ceb1138bb52915f5409cf25d2b1b86c
James <James@PowerDog64Bit>
2014-09-18 01:32:37 +0800

    JAmes Working on MPP

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/StaticClasses/Helper/LoadJamesTestData.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/StaticClasses/Helper/LoadJamesTestData.java
@@ -7,12 +7,12 @@ package IslandFurniture.StaticClasses.Helper;
 
 import IslandFurniture.EJB.Entities.FurnitureModel;
 import IslandFurniture.EJB.Entities.ManufacturingFacility;
-import IslandFurniture.EJB.Entities.Month;
 import IslandFurniture.EJB.Entities.MonthlyStockSupplyReq;
 import IslandFurniture.EJB.Entities.ProductionCapacity;
 import IslandFurniture.EJB.Entities.StockSupplied;
 import IslandFurniture.EJB.Entities.Store;
 import IslandFurniture.EJB.Manufacturing.ManageProductionPlanningRemote;
+import IslandFurniture.EJB.SalesPlanning.SalesForecastBeanLocal;
 import java.util.List;
 import java.util.Random;
 import javax.ejb.EJB;
@@ -23,6 +23,7 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
@@ -38,40 +39,69 @@ public class LoadJamesTestData implements LoadJamesTestDataRemote {
 
     @EJB
     private ManageProductionPlanningRemote mpp;
+    
+
 
     public void persist(Object object) {
         em.persist(object);
     }
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.NEVER)
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Boolean loadProductionCapacityData() {
-        try{
-        Query q = em.createQuery("select MF from ManufacturingFacility MF");
-        for (ManufacturingFacility MF : (List<ManufacturingFacility>) q.getResultList()) {
-            Query L = em.createQuery("SELECT SS from StockSupplied SS where SS.manufacturingFacility=:mf");
-            L.setParameter("mf", MF);
+        try {
+            Query q = em.createQuery("select MF from ManufacturingFacility MF");
+            for (ManufacturingFacility MF : (List<ManufacturingFacility>) q.getResultList()) {
+                Query L = em.createQuery("SELECT SS from StockSupplied SS where SS.manufacturingFacility=:mf");
+                L.setParameter("mf", MF);
 
-            for (StockSupplied SS : (List<StockSupplied>) L.getResultList()) {
-                
-                if (!(SS.getStock() instanceof FurnitureModel)) continue;
+                for (StockSupplied SS : (List<StockSupplied>) L.getResultList()) {
 
-                Query M = em.createQuery("SELECT MAX(MSSR.qtyRequested) from MonthlyStockSupplyReq MSSR where MSSR.stock=:stk and MSSR.store=:store");
-                M.setParameter("store", SS.getStore());
-                M.setParameter("stk", SS.getStock());
-                int max = (int) M.getSingleResult();
-                Random r=new Random();
-                int Capacity=(int)max*(10+r.nextInt(5));
-                mpp.createOrUpdateCapacity(SS.getStock().getName(), MF.getName(),Capacity);
-                System.out.println("loadProductionCapacityData(): MF="+MF.getName()+" PRODUCT="+SS.getStock().getName()+" MaxCapacity="+Capacity);
+                    if (!(SS.getStock() instanceof FurnitureModel)) {
+                        continue;
+                    }
+
+                    Query M = em.createQuery("SELECT MAX(MSSR.qtyRequested) from MonthlyStockSupplyReq MSSR where MSSR.stock=:stk and MSSR.store=:store");
+                    M.setParameter("store", SS.getStore());
+                    M.setParameter("stk", SS.getStock());
+                    int max = (int) M.getSingleResult();
+                    Random r = new Random();
+                    int Capacity = (int) max * (10 + r.nextInt(5));
+                    mpp.createOrUpdateCapacity(SS.getStock().getName(), MF.getName(), Capacity);
+
+                //Create a few fake MSSR
+                    for (int i = 2; i <= 12; i++) {
+                        if (Math.random() > 0.5) {
+                            continue;
+                        }
+
+                        int i_month = Helper.addMonth(Helper.getCurrentMonth(), Helper.getCurrentYear(), i, true);
+                        int i_year = Helper.addMonth(Helper.getCurrentMonth(), Helper.getCurrentYear(), i, false);
+                        
+
+                        MonthlyStockSupplyReq MSSR=new MonthlyStockSupplyReq();
+                        MSSR.setMonth(Helper.translateMonth(i_month));
+                        MSSR.setYear(i_year);
+                        MSSR.setStock(SS.getStock());
+                        MSSR.setStore(SS.getStore());
+                        MSSR.setQtyRequested(r.nextInt(max)+max/4); //Max of 1.25
+                        
+                        try{
+                        em.persist(MSSR);
+                        
+                        }catch(Exception ex){}
+                        
+                    }
+
+                    System.out.println("loadProductionCapacityData(): MF=" + MF.getName() + " PRODUCT=" + SS.getStock().getName() + " MaxCapacity=" + Capacity);
+
+                }
 
             }
-
-          
+            return true;
+        } catch (Exception ex) {
+            return false;
         }
-        return true;
-        }catch(Exception ex){return false;}
     }
-    
-    
+
 }
