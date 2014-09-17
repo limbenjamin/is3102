e9fa4f6eaee4cbf944e07d3270ba3fb452305bc1
James <James@PowerDog64Bit>
2014-09-18 00:40:39 +0800

    JAmes Working on MPP

--- IslandFurnitureJamesTestClient/src/java/IslandFurniture/WAR/Manufacturing/ViewProductionPlanning.java
+++ IslandFurnitureJamesTestClient/src/java/IslandFurniture/WAR/Manufacturing/ViewProductionPlanning.java
@@ -5,12 +5,10 @@
  */
 package IslandFurniture.WAR.Manufacturing;
 
-import IslandFurniture.EJB.Entities.ManufacturingFacility;
-import IslandFurniture.EJB.Entities.Month;
-import IslandFurniture.EJB.Entities.MonthlyProcurementPlan;
-import IslandFurniture.EJB.Entities.MonthlyProductionPlan;
+
+import IslandFurniture.EJB.Manufacturing.MaterialResourcePlanningView;
 import IslandFurniture.EJB.Manufacturing.ManageProductionPlanningRemote;
-import IslandFurniture.StaticClasses.Helper.Helper;
+import IslandFurniture.WAR.HELPER.helper;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -26,6 +24,7 @@ import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import org.primefaces.component.datatable.DataTable;
 
 @SessionScoped
 @ManagedBean(name = "DemandPlanning")
@@ -34,20 +33,17 @@ public class ViewProductionPlanning implements Serializable {
     @EJB
     private ManageProductionPlanningRemote mpp;
 
-    @PersistenceContext(unitName = "IslandFurniture")
-    private EntityManager em;
+    @EJB
+    MaterialResourcePlanningView dpv;
 
     private List<SelectItem> MF_LIST;
     private String MF;
+    private List<ColumnModel> dates_columns=new ArrayList<ColumnModel> ();
 
     @PostConstruct
     public void init() {
         System.out.println("ViewProductionPlanning() Init!");
-        MF_LIST = new ArrayList<SelectItem>();
-        Query q = em.createQuery("Select MF from ManufacturingFacility MF ORDER BY MF.country.name");
-        for (ManufacturingFacility MF : (List<ManufacturingFacility>) q.getResultList()) {
-            MF_LIST.add(new SelectItem(MF.getName(), MF.getCountry().getName() + "/" + MF.getName()));
-        }
+        MF_LIST = helper.convertHMToDropdown(dpv.getAuthorizedMF("AUTH_CHECK_TO_IMPLEMENT_SOON"));
     }
 
     public List<SelectItem> getMF_LIST() {
@@ -59,83 +55,52 @@ public class ViewProductionPlanning implements Serializable {
     }
 
     public void setMF(String MF) {
-        mpp.setCN(MF);
-        System.out.println("ViewProductionPlanning() MF Changed to " + MF);
+
         this.MF = MF;
 
     }
 
-    public String getPsHTML() {
-        int m = Helper.addMonth(Helper.getCurrentMonth(), Helper.getCurrentYear(), 6, true);
-        int y = Helper.addMonth(Helper.getCurrentMonth(), Helper.getCurrentYear(), 6, false);
-        String html = "";
-        Query q = em.createQuery("select MPP from MonthlyProductionPlan MPP where ((MPP.month+1)+MPP.year*12)<=(:m+1)+:y*12 and MPP.locked=false order by MPP.furnitureModel.name , ((MPP.month+1)+MPP.year*12 DESC)"); //                //em.createNamedQuery("MonthlyProductionPlan.FindUntilAllModel");
-        q.setParameter("m", m);
-        q.setParameter("y", y);
-        String fm = "";
-        String dump = "";
-        for (MonthlyProductionPlan mppi : (List<MonthlyProductionPlan>) q.getResultList()) {
-            if (mppi.getFurnitureModel().getName() != fm) {
-                fm = mppi.getFurnitureModel().getName();
-                html += "<h2>Planning for " + fm + "</h2>";
+    public void mfListerner() {
+        try {
+            mpp.setMF(this.MF);
+        } catch (Exception ex) {
+        }
+        //Create the Datatable
+        dates_columns.clear();
+        dates_columns.add(new ColumnModel("A", "a"));
+        System.out.println("ViewProductionPlanning() MF Changed to " + MF);
 
-                //Create rows
-                dump = "";
-                Month c_m = null;
-                try {
-                    c_m = Helper.translateMonth(m);
-                } catch (Exception ex) {
-                }
+    }
 
-                int c_y = y;
+    public void automaticPlanning() {
+        System.out.println("ViewProductionPlanning() User Triggered Production Planning Algorithm");
+        try {
+            mpp.CreateProductionPlanFromForecast();
+        } catch (Exception ex) {
+        }
+    }
 
-                while (!(c_m == Helper.getCurrentMonth() && c_y == Helper.getCurrentYear())) {
+    public List<ColumnModel> getDates_columns() {
+        return dates_columns;
+    }
 
-                    try {
-                        c_m = Helper.translateMonth(Helper.addMonth(c_m, y, -1, true));
-                    } catch (Exception ex) {
-                    }
-                    c_y = Helper.addMonth(c_m, c_y, -1, false);
-                    dump = "<th>" + c_m.toString() + "/" + c_y + "</th>" + dump;
-                }
-                dump = "<table border='1'><tr><th style='width:200px'></th>" + dump + "</tr>";
-                MonthlyProductionPlan temp = mppi;
-                String r = "";
-                while (temp != null) {
-                    r = "<td>" + temp.getTotalDemand(em) + "</td>" + r;
-                    temp = temp.getPrevMonthlyProductionPlan(em);
-                }
-                r = "<tr><td>Total Required:</td>" + r + "</tr>";
-                dump += r;
+    static public class ColumnModel implements Serializable {
 
-                temp = mppi;
-                r = "";
-                while (temp != null) {
-                    r = "<td>" + temp.getQTY() + "</td>" + r;
-                    temp = temp.getPrevMonthlyProductionPlan(em);
-                }
-                r = "<tr><td>Planned Production:</td>" + r + "</tr>";
-                dump += r;
+        private String header;
+        private String property;
 
-                temp = mppi;
-                r = "";
-                while (temp != null) {
-                    
-                    
-                    r = "<td>" + temp.getManufacturingFacility().getCurrentFreeCapacity(em, temp.getMonth(), temp.getYear()) + "</td>" + r;
-                    temp = temp.getPrevMonthlyProductionPlan(em);
-                }
-                r = "<tr><td>Factory Available Capacity:</td>" + r + "</tr>";
-                dump += r;
-
-                dump += "</table>";
-
-                html += dump;
-            }
-
+        public ColumnModel(String header, String property) {
+            this.header = header;
+            this.property = property;
         }
 
-        return html;
+        public String getHeader() {
+            return header;
+        }
+
+        public String getProperty() {
+            return property;
+        }
     }
 
 }
