993e3538f2a213cbe3d900812a93f54bbbbcef56
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-09-19 02:37:11 +0800

    Added and editing InventoryTransfer

---
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SupplyChain/InventoryTransferManagedBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IslandFurniture.WAR.SupplyChain;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author KamilulAshraf
+ */
+@Named(value = "inventoryTransferManagedBean")
+@Dependent
+public class InventoryTransferManagedBean {
+
+    /**
+     * Creates a new instance of InventoryTransferManagedBean
+     */
+    public InventoryTransferManagedBean() {
+    }
+    
+}
