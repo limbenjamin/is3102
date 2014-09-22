819e84dda3b8d249e61382bb58b33d8654153485
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-09-22 20:06:19 +0800

    Minor changes on Goods Issued

--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SupplyChain/GoodsIssuedDocumentManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SupplyChain/GoodsIssuedDocumentManagedBean.java
@@ -116,7 +116,14 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
         if (goodsIssuedDocument.getIssuedDate() != null) {
             issuedDateString = df.format(goodsIssuedDocument.getIssuedDate().getTime());
         }
+        
+        if (goodsIssuedDocument.getDeliverTo() != null) {
+            plantId = goodsIssuedDocument.getDeliverTo().getId();
+        }
+        
+        plantList = mgrl.viewPlant(plant);
         System.out.println("Init");
+        
     }
 
     public String krefresh() {
@@ -130,13 +137,15 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
     public String editGoodsIssuedDocument(ActionEvent event) throws ParseException {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("GRDid", event.getComponent().getAttributes().get("GRDid"));
         GoodsIssuedDocument grd = (GoodsIssuedDocument) event.getComponent().getAttributes().get("grd");
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("plantId", event.getComponent().getAttributes().get("plantId"));
+        plantId = (Long) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("plantId");
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("date", event.getComponent().getAttributes().get("date"));
         issuedDateString = (String) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("date");
         issuedDateType = new SimpleDateFormat("yyyy-MM-dd").parse(issuedDateString);
         Calendar issuedDateCal = Calendar.getInstance();
         Date date = issuedDateType;
         issuedDateCal.setTime(date);
-        mgrl.editGoodsIssuedDocument(grd.getId(), issuedDateCal);
+        mgrl.editGoodsIssuedDocument(grd.getId(), issuedDateCal, plantId);
         return "goodsissueddocument";
     }
 
@@ -190,4 +199,270 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
         FacesContext.getCurrentInstance().getExternalContext().redirect("goodsissueddocumentposted.xhtml");
     }
 
+    public Long getPlantId() {
+        return plantId;
+    }
+
+    public void setPlantId(Long plantId) {
+        this.plantId = plantId;
+    }
+
+    public Long getGoodsIssuedDocumentId() {
+        return goodsIssuedDocumentId;
+    }
+
+    public void setGoodsIssuedDocumentId(Long goodsIssuedDocumentId) {
+        this.goodsIssuedDocumentId = goodsIssuedDocumentId;
+    }
+
+    public Long getGoodsIssuedDocumentDetailId() {
+        return goodsIssuedDocumentDetailId;
+    }
+
+    public void setGoodsIssuedDocumentDetailId(Long goodsIssuedDocumentDetailId) {
+        this.goodsIssuedDocumentDetailId = goodsIssuedDocumentDetailId;
+    }
+
+    public Long getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Long stockId) {
+        this.stockId = stockId;
+    }
+
+    public Long getStorageBinId() {
+        return storageBinId;
+    }
+
+    public void setStorageBinId(Long storageBinId) {
+        this.storageBinId = storageBinId;
+    }
+
+    public Long getStorageAreaid() {
+        return storageAreaid;
+    }
+
+    public void setStorageAreaid(Long storageAreaid) {
+        this.storageAreaid = storageAreaid;
+    }
+
+    public Long getStockUnitId() {
+        return stockUnitId;
+    }
+
+    public void setStockUnitId(Long stockUnitId) {
+        this.stockUnitId = stockUnitId;
+    }
+
+    public String getIssuedDateString() {
+        return issuedDateString;
+    }
+
+    public void setIssuedDateString(String issuedDateString) {
+        this.issuedDateString = issuedDateString;
+    }
+
+    public Date getIssuedDateType() {
+        return issuedDateType;
+    }
+
+    public void setIssuedDateType(Date issuedDateType) {
+        this.issuedDateType = issuedDateType;
+    }
+
+    public DateFormat getDf() {
+        return df;
+    }
+
+    public void setDf(DateFormat df) {
+        this.df = df;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDeliverynote() {
+        return deliverynote;
+    }
+
+    public void setDeliverynote(String deliverynote) {
+        this.deliverynote = deliverynote;
+    }
+
+    public Calendar getPostingDate() {
+        return postingDate;
+    }
+
+    public void setPostingDate(Calendar postingDate) {
+        this.postingDate = postingDate;
+    }
+
+    public Calendar getIssuedDate() {
+        return issuedDate;
+    }
+
+    public void setIssuedDate(Calendar issuedDate) {
+        this.issuedDate = issuedDate;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<GoodsIssuedDocument> getGoodsIssuedDocumentList() {
+        return goodsIssuedDocumentList;
+    }
+
+    public void setGoodsIssuedDocumentList(List<GoodsIssuedDocument> goodsIssuedDocumentList) {
+        this.goodsIssuedDocumentList = goodsIssuedDocumentList;
+    }
+
+    public List<GoodsIssuedDocumentDetail> getGoodsIssuedDocumentDetailList() {
+        return goodsIssuedDocumentDetailList;
+    }
+
+    public void setGoodsIssuedDocumentDetailList(List<GoodsIssuedDocumentDetail> goodsIssuedDocumentDetailList) {
+        this.goodsIssuedDocumentDetailList = goodsIssuedDocumentDetailList;
+    }
+
+    public List<StockUnit> getStockUnitList() {
+        return stockUnitList;
+    }
+
+    public void setStockUnitList(List<StockUnit> stockUnitList) {
+        this.stockUnitList = stockUnitList;
+    }
+
+    public List<StockUnit> getStockUnitMainList() {
+        return stockUnitMainList;
+    }
+
+    public void setStockUnitMainList(List<StockUnit> stockUnitMainList) {
+        this.stockUnitMainList = stockUnitMainList;
+    }
+
+    public List<StorageBin> getStorageBinList() {
+        return storageBinList;
+    }
+
+    public void setStorageBinList(List<StorageBin> storageBinList) {
+        this.storageBinList = storageBinList;
+    }
+
+    public List<StorageArea> getStorageAreaList() {
+        return storageAreaList;
+    }
+
+    public void setStorageAreaList(List<StorageArea> storageAreaList) {
+        this.storageAreaList = storageAreaList;
+    }
+
+    public List<Plant> getPlantList() {
+        return plantList;
+    }
+
+    public void setPlantList(List<Plant> plantList) {
+        this.plantList = plantList;
+    }
+
+    public GoodsIssuedDocument getGoodsIssuedDocument() {
+        return goodsIssuedDocument;
+    }
+
+    public void setGoodsIssuedDocument(GoodsIssuedDocument goodsIssuedDocument) {
+        this.goodsIssuedDocument = goodsIssuedDocument;
+    }
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+    public Plant getPlant() {
+        return plant;
+    }
+
+    public void setPlant(Plant plant) {
+        this.plant = plant;
+    }
+
+    public StorageBin getStorageBin() {
+        return storageBin;
+    }
+
+    public void setStorageBin(StorageBin storageBin) {
+        this.storageBin = storageBin;
+    }
+
+    public StockUnit getStockUnit() {
+        return stockUnit;
+    }
+
+    public void setStockUnit(StockUnit stockUnit) {
+        this.stockUnit = stockUnit;
+    }
+
+    public ManageGoodsIssuedLocal getMgrl() {
+        return mgrl;
+    }
+
+    public void setMgrl(ManageGoodsIssuedLocal mgrl) {
+        this.mgrl = mgrl;
+    }
+
+    public ManageInventoryMovementLocal getMsul() {
+        return msul;
+    }
+
+    public void setMsul(ManageInventoryMovementLocal msul) {
+        this.msul = msul;
+    }
+
+    public ManageStorageLocationLocal getMsll() {
+        return msll;
+    }
+
+    public void setMsll(ManageStorageLocationLocal msll) {
+        this.msll = msll;
+    }
+
+    public ManageUserAccountBeanLocal getStaffBean() {
+        return staffBean;
+    }
+
+    public void setStaffBean(ManageUserAccountBeanLocal staffBean) {
+        this.staffBean = staffBean;
+    }
+
+    public ManageInventoryMonitoringLocal getMiml() {
+        return miml;
+    }
+
+    public void setMiml(ManageInventoryMonitoringLocal miml) {
+        this.miml = miml;
+    }
+
+    
+    
   }
