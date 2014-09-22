4cf578d514607ffc14f87c1b0804d04d23368750
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-09-22 19:27:59 +0800

    Minor changes on Goods Receipt and Goods Issued

--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SupplyChain/GoodsIssuedDocumentManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SupplyChain/GoodsIssuedDocumentManagedBean.java
@@ -16,6 +16,7 @@ import IslandFurniture.EJB.SupplyChain.ManageStorageLocationLocal;
 import IslandFurniture.WAR.CommonInfrastructure.Util;
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -48,6 +49,7 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
 
     private String issuedDateString;
     private Date issuedDateType;
+    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     private String username;
     private String deliverynote;
@@ -65,6 +67,7 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
 
     private List<StorageBin> storageBinList;
     private List<StorageArea> storageAreaList;
+    private List<Plant> plantList;
 
     private GoodsIssuedDocument goodsIssuedDocument;
     private Staff staff;
@@ -103,8 +106,6 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
             goodsIssuedDocument = mgrl.getGoodsIssuedDocument(goodsIssuedDocumentId);
         }
 
-//      Ask Ben = To ask on how to display current date
-//      issuedDateString = goodsIssuedDocument.getPostingDate().getTime().toString();
         System.out.println("GoodsIssuedDocumentId: " + goodsIssuedDocumentId);
         goodsIssuedDocument = mgrl.getGoodsIssuedDocument(goodsIssuedDocumentId);
         storageBinList = mgrl.viewStorageBin(plant);
@@ -112,6 +113,9 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
         goodsIssuedDocumentList = mgrl.viewGoodsIssuedDocumentIndividual(goodsIssuedDocument);
         stockUnitList = mgrl.viewStockUnit(plant);
         stockUnitMainList = mgrl.viewStockUnitByIdMain(plant, goodsIssuedDocument);
+        if (goodsIssuedDocument.getIssuedDate() != null) {
+            issuedDateString = df.format(goodsIssuedDocument.getIssuedDate().getTime());
+        }
         System.out.println("Init");
     }
 
@@ -166,18 +170,17 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("GRDid", goodsIssuedDocumentId);
         FacesContext.getCurrentInstance().getExternalContext().redirect("goodsissueddocumentcommit.xhtml");
     }
-    
-    
+
     public void addGoodsIssuedDocumentStockUnit(ActionEvent event) throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("GRDid", event.getComponent().getAttributes().get("GRDid"));
         goodsIssuedDocumentId = (Long) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("GRDid");
-        
+
         Calendar cal = Calendar.getInstance();
         Date date = new Date();
         cal.setTime(date);
         postingDate = cal;
-        
-        for (StockUnit g : stockUnitMainList) {       
+
+        for (StockUnit g : stockUnitMainList) {
             mgrl.createGoodsIssuedDocumentDetail(goodsIssuedDocumentId, g.getStock().getId(), g.getQty());
             mgrl.editGoodsIssuedDocument2(goodsIssuedDocumentId, postingDate);
             msul.deleteStockUnit(g.getId());
@@ -187,254 +190,4 @@ public class GoodsIssuedDocumentManagedBean implements Serializable {
         FacesContext.getCurrentInstance().getExternalContext().redirect("goodsissueddocumentposted.xhtml");
     }
 
-    public Long getPlantId() {
-        return plantId;
-    }
-
-    public void setPlantId(Long plantId) {
-        this.plantId = plantId;
-    }
-
-    public Long getGoodsIssuedDocumentId() {
-        return goodsIssuedDocumentId;
-    }
-
-    public void setGoodsIssuedDocumentId(Long goodsIssuedDocumentId) {
-        this.goodsIssuedDocumentId = goodsIssuedDocumentId;
-    }
-
-    public Long getGoodsIssuedDocumentDetailId() {
-        return goodsIssuedDocumentDetailId;
-    }
-
-    public void setGoodsIssuedDocumentDetailId(Long goodsIssuedDocumentDetailId) {
-        this.goodsIssuedDocumentDetailId = goodsIssuedDocumentDetailId;
-    }
-
-    public Long getStockId() {
-        return stockId;
-    }
-
-    public void setStockId(Long stockId) {
-        this.stockId = stockId;
-    }
-
-    public Long getStorageBinId() {
-        return storageBinId;
-    }
-
-    public void setStorageBinId(Long storageBinId) {
-        this.storageBinId = storageBinId;
-    }
-
-    public Long getStorageAreaid() {
-        return storageAreaid;
-    }
-
-    public void setStorageAreaid(Long storageAreaid) {
-        this.storageAreaid = storageAreaid;
-    }
-
-    public Long getStockUnitId() {
-        return stockUnitId;
-    }
-
-    public void setStockUnitId(Long stockUnitId) {
-        this.stockUnitId = stockUnitId;
-    }
-
-    public String getIssuedDateString() {
-        return issuedDateString;
-    }
-
-    public void setIssuedDateString(String issuedDateString) {
-        this.issuedDateString = issuedDateString;
-    }
-
-    public Date getIssuedDateType() {
-        return issuedDateType;
-    }
-
-    public void setIssuedDateType(Date issuedDateType) {
-        this.issuedDateType = issuedDateType;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getDeliverynote() {
-        return deliverynote;
-    }
-
-    public void setDeliverynote(String deliverynote) {
-        this.deliverynote = deliverynote;
-    }
-
-    public Calendar getPostingDate() {
-        return postingDate;
-    }
-
-    public void setPostingDate(Calendar postingDate) {
-        this.postingDate = postingDate;
-    }
-
-    public Calendar getIssuedDate() {
-        return issuedDate;
-    }
-
-    public void setIssuedDate(Calendar issuedDate) {
-        this.issuedDate = issuedDate;
-    }
-
-    public Stock getStock() {
-        return stock;
-    }
-
-    public void setStock(Stock stock) {
-        this.stock = stock;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public List<GoodsIssuedDocument> getGoodsIssuedDocumentList() {
-        return goodsIssuedDocumentList;
-    }
-
-    public void setGoodsIssuedDocumentList(List<GoodsIssuedDocument> goodsIssuedDocumentList) {
-        this.goodsIssuedDocumentList = goodsIssuedDocumentList;
-    }
-
-    public List<GoodsIssuedDocumentDetail> getGoodsIssuedDocumentDetailList() {
-        return goodsIssuedDocumentDetailList;
-    }
-
-    public void setGoodsIssuedDocumentDetailList(List<GoodsIssuedDocumentDetail> goodsIssuedDocumentDetailList) {
-        this.goodsIssuedDocumentDetailList = goodsIssuedDocumentDetailList;
-    }
-
-    public List<StockUnit> getStockUnitList() {
-        return stockUnitList;
-    }
-
-    public void setStockUnitList(List<StockUnit> stockUnitList) {
-        this.stockUnitList = stockUnitList;
-    }
-
-    public List<StockUnit> getStockUnitMainList() {
-        return stockUnitMainList;
-    }
-
-    public void setStockUnitMainList(List<StockUnit> stockUnitMainList) {
-        this.stockUnitMainList = stockUnitMainList;
-    }
-
-    public List<StorageBin> getStorageBinList() {
-        return storageBinList;
-    }
-
-    public void setStorageBinList(List<StorageBin> storageBinList) {
-        this.storageBinList = storageBinList;
-    }
-
-    public List<StorageArea> getStorageAreaList() {
-        return storageAreaList;
-    }
-
-    public void setStorageAreaList(List<StorageArea> storageAreaList) {
-        this.storageAreaList = storageAreaList;
-    }
-
-    public GoodsIssuedDocument getGoodsIssuedDocument() {
-        return goodsIssuedDocument;
-    }
-
-    public void setGoodsIssuedDocument(GoodsIssuedDocument goodsIssuedDocument) {
-        this.goodsIssuedDocument = goodsIssuedDocument;
-    }
-
-    public Staff getStaff() {
-        return staff;
-    }
-
-    public void setStaff(Staff staff) {
-        this.staff = staff;
-    }
-
-    public Plant getPlant() {
-        return plant;
-    }
-
-    public void setPlant(Plant plant) {
-        this.plant = plant;
-    }
-
-    public StorageBin getStorageBin() {
-        return storageBin;
-    }
-
-    public void setStorageBin(StorageBin storageBin) {
-        this.storageBin = storageBin;
-    }
-
-    public StockUnit getStockUnit() {
-        return stockUnit;
-    }
-
-    public void setStockUnit(StockUnit stockUnit) {
-        this.stockUnit = stockUnit;
-    }
-
-    public ManageGoodsIssuedLocal getMgrl() {
-        return mgrl;
-    }
-
-    public void setMgrl(ManageGoodsIssuedLocal mgrl) {
-        this.mgrl = mgrl;
-    }
-
-    public ManageInventoryMovementLocal getMsul() {
-        return msul;
-    }
-
-    public void setMsul(ManageInventoryMovementLocal msul) {
-        this.msul = msul;
-    }
-
-    public ManageStorageLocationLocal getMsll() {
-        return msll;
-    }
-
-    public void setMsll(ManageStorageLocationLocal msll) {
-        this.msll = msll;
-    }
-
-    public ManageUserAccountBeanLocal getStaffBean() {
-        return staffBean;
-    }
-
-    public void setStaffBean(ManageUserAccountBeanLocal staffBean) {
-        this.staffBean = staffBean;
-    }
-
-    public ManageInventoryMonitoringLocal getMiml() {
-        return miml;
-    }
-
-    public void setMiml(ManageInventoryMonitoringLocal miml) {
-        this.miml = miml;
-    }
-    
-    
-
-}
+  }
