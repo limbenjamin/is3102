d6bd83f93e1dd448c16f4c21d8bb3d876e4132b4
Benjamin Lim <mail@limbenjamin.com>
2014-09-23 16:50:51 +0800

    Monthly procurement plan completed. Unable to verify correctness by hand

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Manufacturing/ManageProcurementPlan.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Manufacturing/ManageProcurementPlan.java
@@ -6,14 +6,19 @@
 
 package IslandFurniture.EJB.Manufacturing;
 
+import IslandFurniture.EJB.Entities.CountryOffice;
 import IslandFurniture.EJB.Entities.ManufacturingFacility;
+import IslandFurniture.EJB.Entities.Month;
 import IslandFurniture.EJB.Entities.MonthlyProcurementPlan;
+import IslandFurniture.EJB.Entities.MonthlyProcurementPlanPK;
 import IslandFurniture.EJB.Entities.MonthlyStockSupplyReq;
 import IslandFurniture.EJB.Entities.ProcuredStock;
 import IslandFurniture.EJB.Entities.RetailItem;
-import IslandFurniture.EJB.Entities.StockSupplied;
+import IslandFurniture.EJB.ITManagement.ManageOrganizationalHierarchyBeanLocal;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -27,39 +32,55 @@ import javax.persistence.Query;
 public class ManageProcurementPlan implements ManageProcurementPlanLocal {
 
     private List<MonthlyStockSupplyReq> mssrList;
-    private MonthlyProcurementPlan mpp;
     private MonthlyStockSupplyReq mssr;
+    private CountryOffice co;
+    private ManufacturingFacility mf;
+    private MonthlyProcurementPlan mpp;
+    private Month month;
     private ProcuredStock procuredStock;
     private RetailItem retailItem;
-    private ManufacturingFacility mf;
-    private StockSupplied ss;
     
     @PersistenceContext(unitName = "IslandFurniture")
     private EntityManager em;
+    
+    @EJB
+    private ManageOrganizationalHierarchyBeanLocal mohb;
     
     @Override
     public void createMonthlyProcumentPlan(){
         Query query = em.createQuery("SELECT m FROM MonthlyStockSupplyReq m WHERE m.stock.id IN (SELECT p.id FROM ProcuredStock p)");
         mssrList = query.getResultList();
         Iterator<MonthlyStockSupplyReq> iterator = mssrList.iterator();
-        while (iterator.hasNext()) {
-            mpp = new MonthlyProcurementPlan();
+        while(iterator.hasNext()){
             mssr = iterator.next();
-            mpp.setMonth(mssr.getMonth());
-            mpp.setYear(mssr.getYear());
-            mpp.setQty(mssr.getQtyRequested());
-            mpp.setLocked(false);
-            procuredStock = (ProcuredStock) mssr.getStock();
-            retailItem = (RetailItem) procuredStock;
-            mpp.setRetailItem(retailItem);
             query = em.createQuery("Select ss.manufacturingFacility FROM StockSupplied ss WHERE ss.countryOffice.id=:co AND ss.stock.id=:stock");
             query.setParameter("co", mssr.getCountryOffice().getId());
             query.setParameter("stock", mssr.getStock().getId());
-            mf = (ManufacturingFacility) query.getSingleResult(); 
-            mpp.setManufacturingFacility(mf);
-            em.persist(mpp);
-	}
+            mf = (ManufacturingFacility) query.getSingleResult();
+            month= mssr.getMonth();
+            Integer year = mssr.getYear();
+            mpp = em.find(MonthlyProcurementPlan.class, new MonthlyProcurementPlanPK(mf.getId(),mssr.getStock().getId(),month,year));
+            if (mpp == null){
+                mpp = new MonthlyProcurementPlan();
+                mpp.setMonth(mssr.getMonth());
+                mpp.setYear(mssr.getYear());
+                procuredStock = (ProcuredStock) mssr.getStock();
+                retailItem = (RetailItem) procuredStock;
+                mpp.setRetailItem(retailItem);
+                mpp.setManufacturingFacility(mf);
+                mpp.setLocked(Boolean.FALSE);
+                mpp.setQty(mssr.getQtyRequested());
+                em.persist(mpp);
+            }
+            //MPP already exists, created by another country which uses the same MF and orders the same stock
+            else{
+                mpp.setQty(mpp.getQty()+mssr.getQtyRequested());
+                em.merge(mpp);
+            }
+        }
     }
+    
+    
     
     @Override
     public List<MonthlyProcurementPlan> viewMonthlyProcurementPlan(){
