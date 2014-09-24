90a79c24087e522ceb9eb06af5666a2ad23d4e7f
Benjamin Lim <mail@limbenjamin.com>
2014-09-24 21:42:49 +0800

    redirecting for reset password done

--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/ITManagement/ResetPasswordManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/ITManagement/ResetPasswordManagedBean.java
@@ -7,13 +7,18 @@
 package IslandFurniture.WAR.ITManagement;
 
 import IslandFurniture.EJB.CommonInfrastructure.ManageAuthenticationBeanLocal;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
+import javax.servlet.http.HttpServletRequest;
 
 
 
@@ -43,14 +48,24 @@ public class ResetPasswordManagedBean {
     
     @PostConstruct
     public void init(){
+        String uri = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getRequestURI();
+        if (uri.contains("resetpassword.xhtml")){
+            code = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("code");
+            if (code == null){
+                ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+                try {
+                    ec.redirect("../login.xhtml");
+                } catch (IOException ex) {
+                    Logger.getLogger(ResetPasswordManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        System.err.println(uri);
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("message",
 	                    new FacesMessage(FacesMessage.SEVERITY_INFO, "",""));
-        try{
-            code = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("code");
-        }catch (Exception e){
-
-        }
     }
+    
+
     
     public String forgotpassword() {
         boolean result = authBean.forgetPassword(username);
@@ -67,21 +82,36 @@ public class ResetPasswordManagedBean {
     }
     
     
-    public void resetpassword() {
+    public String resetpassword() {
+
         if (password.equals(confirmPassword)){
             boolean result = authBean.resetPassword(code,password);
             Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
             if (result) {
                 FacesContext.getCurrentInstance().getExternalContext().getFlash().put("message",
 	                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Password Reset successful",""));
+                ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+                try {
+                    ec.redirect("../login.xhtml");
+                } catch (IOException ex) {
+                    Logger.getLogger(ResetPasswordManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }else{
                 FacesContext.getCurrentInstance().getExternalContext().getFlash().put("message",
 	                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error, unable to reset password",""));
             }
+            return "login";
         }else{
-             FacesContext.getCurrentInstance().getExternalContext().getFlash().put("message",
+            FacesContext.getCurrentInstance().getExternalContext().getFlash().put("message",
 	                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error, passwords not the same",""));
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            try {
+                ec.redirect("resetpassword.xhtml?code="+code);
+            } catch (IOException ex) {
+                Logger.getLogger(ResetPasswordManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        return "login";
     }
 
     public String getCode() {
