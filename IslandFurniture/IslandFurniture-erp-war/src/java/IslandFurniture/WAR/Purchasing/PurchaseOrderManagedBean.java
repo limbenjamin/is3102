11a7b27306b010fbb818cc807e35c60f9136a166
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-09-24 17:32:24 +0800

    Made changes to Purchasing Module

--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/Purchasing/PurchaseOrderManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/Purchasing/PurchaseOrderManagedBean.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package IslandFurniture.WAR.Purchasing;
 
 import IslandFurniture.EJB.CommonInfrastructure.ManageUserAccountBeanLocal;
@@ -31,20 +30,21 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author Zee
  */
 @ManagedBean
 @ViewScoped
-public class PurchaseOrderManagedBean implements Serializable{
+public class PurchaseOrderManagedBean implements Serializable {
 
     private String username;
-    
+
     private Long purchaseOrderId;
     private Long supplierId;
     private Long plantId;
-    
+
     private Calendar orderDate;
     private String status;
     private PurchaseOrder purchaseOrder;
@@ -56,14 +56,14 @@ public class PurchaseOrderManagedBean implements Serializable{
     private Supplier supplier;
     private ManufacturingFacility mf;
     private String orderDateString = null;
-    
+
     @EJB
-    private ManageUserAccountBeanLocal staffBean; 
+    private ManageUserAccountBeanLocal staffBean;
     @EJB
     public ManagePurchaseOrderLocal mpol;
     @EJB
     public SupplierManagerLocal sml;
-    
+
     @PostConstruct
     public void init() {
         HttpSession session = Util.getSession();
@@ -78,131 +78,155 @@ public class PurchaseOrderManagedBean implements Serializable{
         plantList = mpol.viewPlants();
         System.out.println("Init");
     }
-    
+
     public String addPurchaseOrder() throws ParseException {
-        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();        
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
         supplierId = Long.parseLong(request.getParameter("createPurchaseOrder:supplierId"));
         supplier = sml.getSupplier(supplierId);
         plantId = staff.getPlant().getId();
         orderDateString = request.getParameter("createPurchaseOrder:orderDateString");
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        Date date = (Date)formatter.parse(orderDateString); 
-        Calendar cal=Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = (Date) formatter.parse(orderDateString);
+        Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        orderDate = cal;        
-        mpol.createNewPurchaseOrder("planned", supplier, plantId, orderDate);
-        return "purchaseorder";
-    }   
-    
+        orderDate = cal;
+        purchaseOrder = mpol.createNewPurchaseOrder("planned", supplier, plantId, orderDate);
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("POid", purchaseOrder.getId());
+        return "purchaseorder2?faces-redirect=true";
+    }
+
     /*public String addNewPurchaseOrder() throws ParseException {
-        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();        
-        supplierId = Long.parseLong(request.getParameter("createPurchaseOrder:supplierId"));
-        supplier = sml.getSupplier(supplierId);
-        plantId = Long.parseLong(request.getParameter("createPurchaseOrder:plantId"));
-        orderDateString = request.getParameter("createPurchaseOrder:orderDateString");
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        Date date = (Date)formatter.parse(orderDateString); 
-        Calendar cal=Calendar.getInstance();
-        cal.setTime(date);
-        orderDate = cal;        
-        mpol.createNewPurchaseOrder("planned", supplier, plantId, orderDate);
-        return "purchaseorder";
-    }*/    
-    
+     HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();        
+     supplierId = Long.parseLong(request.getParameter("createPurchaseOrder:supplierId"));
+     supplier = sml.getSupplier(supplierId);
+     plantId = Long.parseLong(request.getParameter("createPurchaseOrder:plantId"));
+     orderDateString = request.getParameter("createPurchaseOrder:orderDateString");
+     DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+     Date date = (Date)formatter.parse(orderDateString); 
+     Calendar cal=Calendar.getInstance();
+     cal.setTime(date);
+     orderDate = cal;        
+     mpol.createNewPurchaseOrder("planned", supplier, plantId, orderDate);
+     return "purchaseorder";
+     }*/
     public void purchaseOrderDetailActionListener(ActionEvent event) throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("POid", event.getComponent().getAttributes().get("POid"));
         purchaseOrderId = (Long) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("POid");
         System.out.println("this is the purchase order id at MAIN Mgd Bean" + purchaseOrderId);
         FacesContext.getCurrentInstance().getExternalContext().redirect("purchaseorder2.xhtml");
     }
-    
+
     public String deletePurchaseOrder() {
         System.out.println("deleting some order");
         purchaseOrderId = new Long(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("POid"));
         mpol.deletePurchaseOrder(purchaseOrderId);
-        plannedOrderList = mpol.viewPlannedPurchaseOrders();        
+        plannedOrderList = mpol.viewPlannedPurchaseOrders();
         return "purchaseOrder";
-    }    
-    
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public ManufacturingFacility getMf() {
+        return mf;
+    }
+
+    public void setMf(ManufacturingFacility mf) {
+        this.mf = mf;
+    }
+
+    public String getOrderDateString() {
+        return orderDateString;
+    }
+
+    public void setOrderDateString(String orderDateString) {
+        this.orderDateString = orderDateString;
+    }
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }    
-    
+    }
+
     public Long getPlantId() {
         return plantId;
     }
 
     public void setPlantId(Long plantId) {
         this.plantId = plantId;
-    }    
-    
+    }
+
     public Long getSupplierId() {
         return supplierId;
     }
 
     public void setSupplierId(Long supplierId) {
         this.supplierId = supplierId;
-    }    
-    
+    }
+
     public Long getPurchaseOrderId() {
         return purchaseOrderId;
     }
 
     public void setPurchaseOrderId(Long purchaseOrderId) {
         this.purchaseOrderId = purchaseOrderId;
-    }    
-    
+    }
+
     public Calendar getOrderDate() {
         return orderDate;
     }
 
     public void setOrderDate(Calendar orderDate) {
         this.orderDate = orderDate;
-    } 
-    
+    }
+
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
-    }    
-    
+    }
+
     public List<PurchaseOrder> getPlannedOrderList() {
         return plannedOrderList;
     }
 
     public void setPlannedOrderList(List<PurchaseOrder> plannedOrderList) {
         this.plannedOrderList = plannedOrderList;
-    }    
-    
+    }
+
     public PurchaseOrder getPurchaseOrder() {
         return purchaseOrder;
     }
 
     public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
         this.purchaseOrder = purchaseOrder;
-    }    
-    
+    }
+
     public Staff getStaff() {
         return staff;
     }
 
     public void setStaff(Staff staff) {
         this.staff = staff;
-    }   
-    
+    }
+
     public ManagePurchaseOrderLocal getMpol() {
         return mpol;
     }
 
     public void setMgrl(ManagePurchaseOrderLocal mpol) {
         this.mpol = mpol;
-    }    
+    }
 
     public List<PurchaseOrder> getConfirmedOrderList() {
         return confirmedOrderList;
@@ -211,7 +235,6 @@ public class PurchaseOrderManagedBean implements Serializable{
     public void setConfirmedOrderList(List<PurchaseOrder> confirmedOrderList) {
         this.confirmedOrderList = confirmedOrderList;
     }
-
 
     public ManageUserAccountBeanLocal getStaffBean() {
         return staffBean;
@@ -244,5 +267,5 @@ public class PurchaseOrderManagedBean implements Serializable{
     public void setSml(SupplierManagerLocal sml) {
         this.sml = sml;
     }
-    
+
 }
