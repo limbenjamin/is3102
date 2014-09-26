e9ac9bef953dfbe2376ac4a501affdd006afe36d
Chen Tong <chentong@nus.edu.sg>
2014-09-26 17:00:48 +0800

    Implemented production order start (deduct materials based on BOM) and production order completed (add furniture based on order)

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Manufacturing/ProductionManager.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/Manufacturing/ProductionManager.java
@@ -3,15 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package IslandFurniture.EJB.Manufacturing;
 
+import IslandFurniture.EJB.Entities.BOMDetail;
+import IslandFurniture.EJB.Entities.ManufacturingFacility;
 import IslandFurniture.EJB.Entities.ProdOrderStatus;
 import IslandFurniture.EJB.Entities.ProductionOrder;
+import IslandFurniture.EJB.Entities.StockUnit;
+import IslandFurniture.EJB.Entities.StorageAreaType;
+import IslandFurniture.EJB.Entities.StorageBin;
+import IslandFurniture.EJB.Exceptions.InsufficientMaterialException;
+import IslandFurniture.EJB.Exceptions.InvalidInputException;
+import IslandFurniture.EJB.SupplyChain.ManageInventoryMovementLocal;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.REQUIRED;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -19,53 +32,62 @@ import javax.persistence.PersistenceContext;
  */
 @Stateful
 public class ProductionManager implements ProductionManagerLocal {
+    @EJB
+    private ManageInventoryMovementLocal manageInventoryMovement;
 
     @PersistenceContext
     EntityManager em;
     
+    
+
     @Override
     public ProductionOrder deletePO(Long batchNo) {
         ProductionOrder po;
-        
+
         try {
             System.out.println("ProductionManager.deletePO()");
             po = em.find(ProductionOrder.class, batchNo);
             System.out.println("Perform deletion");
             em.remove(po);
             return po;
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.err.println("Error");
             return null;
         }
     }
+
     @Override
     public ProductionOrder editPO(Long batchNo, Calendar date, Integer qty) {
         ProductionOrder po;
         try {
-            System.out.println("ProductionManager.editPO()"); 
+            System.out.println("ProductionManager.editPO()");
             System.out.println("Date is " + date.getTime().toString() + ". Quantity is " + qty);
             po = em.find(ProductionOrder.class, batchNo);
-            if(po == null)
+            if (po == null) {
                 System.out.println("PO is null");
-            if(date != null)
+            }
+            if (date != null) {
                 po.setProdOrderDate(date);
-            if(qty != null)
+            }
+            if (qty != null) {
                 po.setQty(qty);
-            em.persist(po);
+            }
+
             return po;
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.err.println("Error");
-            return null;            
+            return null;
         }
     }
+
     @Override
     public ProductionOrder editCompletedQty(Long batchNo, Integer completedQty) {
         ProductionOrder po;
         try {
             System.out.println("ProductionManager.editCompletedQty()");
             po = em.find(ProductionOrder.class, batchNo);
-            if(completedQty != null) {
-                if(completedQty > po.getQty()) {
+            if (completedQty != null) {
+                if (completedQty > po.getQty()) {
                     System.out.println("Invalid. Completed Quantity cannot be greater than planned quantity");
                     return null;
                 } else {
@@ -74,37 +96,95 @@ public class ProductionManager implements ProductionManagerLocal {
                 }
             }
             return po;
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.err.println("Error");
-            return null;            
+            return null;
         }
     }
+
     @Override
-    public ProductionOrder commencePO(Long batchNo) {
+    @TransactionAttribute(REQUIRED)
+    public ProductionOrder commencePO(Long batchNo, Long binId) throws InvalidInputException, InsufficientMaterialException {
         ProductionOrder po;
-        try {
-            System.out.println("ProductionManager.commencePO()");
-            po = em.find(ProductionOrder.class, batchNo);
-            po.setStatus(ProdOrderStatus.STARTED);
-            em.persist(po);
-            return po;
-        } catch(Exception ex) {
-            System.err.println("Error");
-            return null;             
+        StorageBin sb;
+        System.out.println("ProductionManager.commencePO()");
+
+        po = em.find(ProductionOrder.class, batchNo);
+        sb = em.find(StorageBin.class, binId);
+
+        if (sb != null && po != null) {
+            if (this.checkSufficientMaterials(po, sb)) {
+                for (BOMDetail bomd : po.getFurnitureModel().getBom().getBomDetails()) {
+                    for (Iterator itr = sb.getStockUnits().iterator(); itr.hasNext();) {
+                        StockUnit su = (StockUnit) itr.next();
+
+                        if (su.getStock().equals(bomd.getMaterial())) {
+                            su.setQty(su.getQty() - bomd.getQuantity() * po.getQty());
+
+                            if (su.getQty() <= 0) {
+                                em.remove(su);
+
+                            }
+
+                            break;
+                        }
+                    }
+                }
+                po.setStatus(ProdOrderStatus.STARTED);
+            } else {
+                throw new InsufficientMaterialException("Insufficient Materials in stated bin");
+            }
+        } else {
+            throw new InvalidInputException("No such Production Order or Storage Bin exists!");
         }
+
+        return po;
     }
+
     @Override
-    public ProductionOrder completePO(Long batchNo) {
+    public ProductionOrder completePO(Long batchNo, Long binId) throws InvalidInputException {
         ProductionOrder po;
-        try {
-            System.out.println("ProductionManager.completePO()");
-            po = em.find(ProductionOrder.class, batchNo);
+        StorageBin sb;
+        System.out.println("ProductionManager.completePO()");
+        
+        po = em.find(ProductionOrder.class, batchNo);
+        sb = em.find(StorageBin.class, binId);
+        if(po != null && sb != null){
+            manageInventoryMovement.createStockUnit(po.getFurnitureModel(), po.getBatchNo().toString(), (long) po.getQty(), sb);
             po.setStatus(ProdOrderStatus.COMPLETED);
-            em.persist(po);
+            
             return po;
-        } catch(Exception ex) {
-            System.err.println("Error");
-            return null;             
+        } else{
+            throw new InvalidInputException("Target Storage Bin does not exist!");
         }
+    }
+
+    @Override
+    public List<StorageBin> viewProductionBins(ManufacturingFacility mf) {
+        Query q = em.createNamedQuery("findStorageBinByAreaType");
+        q.setParameter("plant", mf);
+        q.setParameter("saType", StorageAreaType.PRODUCTION);
+
+        return (List<StorageBin>) q.getResultList();
+    }
+
+    private boolean checkSufficientMaterials(ProductionOrder po, StorageBin bin) {
+        for (BOMDetail bomd : po.getFurnitureModel().getBom().getBomDetails()) {
+            boolean hasMaterial = false;
+            for (StockUnit su : bin.getStockUnits()) {
+                if (bomd.getMaterial().equals(su.getStock())) {
+                    if (bomd.getQuantity() * po.getQty() > su.getQty()) {
+                        return false;
+                    } else {
+                        hasMaterial = true;
+                        break;
+                    }
+                }
+            }
+            if (!hasMaterial) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SalesPlanning/CreateForecastManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/SalesPlanning/CreateForecastManagedBean.java
@@ -5,6 +5,7 @@
  */
 package IslandFurniture.WAR.SalesPlanning;
 
+import IslandFurniture.EJB.CommonInfrastructure.ManageNotificationsBeanLocal;
 import IslandFurniture.EJB.CommonInfrastructure.ManageUserAccountBeanLocal;
 import IslandFurniture.EJB.Entities.CountryOffice;
 import IslandFurniture.EJB.Entities.MonthlyStockSupplyReq;
@@ -15,6 +16,7 @@ import IslandFurniture.EJB.Entities.StockSupplied;
 import IslandFurniture.EJB.Exceptions.ForecastFailureException;
 import IslandFurniture.EJB.Exceptions.InvalidInputException;
 import IslandFurniture.EJB.Exceptions.InvalidMssrException;
+import IslandFurniture.EJB.ITManagement.ManageRolesBeanLocal;
 import IslandFurniture.EJB.SalesPlanning.SalesForecastBeanLocal;
 import IslandFurniture.StaticClasses.Helper.Couple;
 import IslandFurniture.WAR.CommonInfrastructure.Util;
@@ -38,6 +40,10 @@ import javax.servlet.http.HttpSession;
 @ManagedBean(name = "createForecastManagedBean")
 @ViewScoped
 public class CreateForecastManagedBean implements Serializable {
+    @EJB
+    private ManageRolesBeanLocal manageRolesBean;
+    @EJB
+    private ManageNotificationsBeanLocal manageNotificationsBean;
 
     @EJB
     private SalesForecastBeanLocal salesForecastBean;
@@ -159,6 +165,8 @@ public class CreateForecastManagedBean implements Serializable {
 
             salesForecastBean.saveMonthlyStockSupplyReq(coupleList);
 
+            manageNotificationsBean.createNewNotificationForRoleFromPlant("Pending Requirements Forecast", "New requirements forecast awaiting your approval", "/salesplanning/reviewforecast.xhtml", "Review Forecast", manageRolesBean.getRoleFromName("Sales Planning (CO)"), co);
+            
             statusMessage = "Forecast saved successfully!";
         } catch (InvalidMssrException ex) {
             statusMessage = "Error saving forecast: " + ex.getMessage();
