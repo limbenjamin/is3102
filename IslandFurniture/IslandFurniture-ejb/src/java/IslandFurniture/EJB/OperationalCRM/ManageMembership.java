3629275d17daa0b028c9dc602b99fadd54a1d7f3
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-10-16 23:52:12 +0800

    Working on Membership - Loyalty Mgmt (OCRM)

---
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/OperationalCRM/ManageMembership.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IslandFurniture.EJB.OperationalCRM;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author KamilulAshraf
+ */
+@Stateless
+@LocalBean
+public class ManageMembership {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
