68ebfbe71ea3005d13d87f93efabba0077c99754
Kamilul Ashraf Kamsani <kamilul@nus.edu.sg>
2014-10-26 13:16:45 +0800

    Working on RedeemableItem, Redemption

---
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/OperationalCRM/RedeemableItemManagedBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IslandFurniture.WAR.OperationalCRM;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author KamilulAshraf
+ */
+@Named(value = "redeemableItemManagedBean")
+@Dependent
+public class RedeemableItemManagedBean {
+
+    /**
+     * Creates a new instance of RedeemableItemManagedBean
+     */
+    public RedeemableItemManagedBean() {
+    }
+    
+}
