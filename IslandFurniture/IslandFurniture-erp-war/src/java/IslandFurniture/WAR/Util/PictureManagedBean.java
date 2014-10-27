8bb7b23d6fee52d9071bae1cf135e5b651006851
yzixuan <aura.chrome@gmail.com>
2014-10-27 12:03:02 +0800

    created a new managed bean called PictureManagedBean for displaying graphic images

--- IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/Util/PictureManagedBean.java
+++ IslandFurniture/IslandFurniture-erp-war/src/java/IslandFurniture/WAR/Util/PictureManagedBean.java
@@ -7,16 +7,17 @@
 package IslandFurniture.WAR.Util;
 
 import IslandFurniture.EJB.OperationalCRM.ManageWebBannerLocal;
+import IslandFurniture.Entities.Picture;
 import IslandFurniture.Entities.WebBanner;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
+import javax.inject.Named;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -28,22 +29,19 @@ import org.primefaces.model.StreamedContent;
 @ApplicationScoped
 public class PictureManagedBean implements Serializable {
     
-    private WebBanner webBanner;   
-    private StreamedContent image;
-    
     @EJB
     private ManageWebBannerLocal bannerBean;
     
     public StreamedContent getImage() throws IOException {
         FacesContext context = FacesContext.getCurrentInstance();
-//        if(context == null) {return null;}
+
         if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
-            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
             return new DefaultStreamedContent();
         } else {
             String pictureId = context.getExternalContext().getRequestParameterMap().get("pictureId");
-            //webBanner = bannerBean.getWebBanner(pictureId);
-            return new DefaultStreamedContent(new ByteArrayInputStream(webBanner.getPicture().getContent()));
+            System.out.println(pictureId);
+            Picture picture = bannerBean.getPicture(Long.valueOf(pictureId)); 
+            return new DefaultStreamedContent(new ByteArrayInputStream(picture.getContent()));
         }
     }    
 }
