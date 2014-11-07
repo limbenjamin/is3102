a606e8aeee37a92c9d5abe93d3201439c4aee4f7
Benjamin Lim <mail@limbenjamin.com>
2014-11-07 15:14:33 +0800

    Merging

--- IslandFurniture/IslandFurniture-webservices-war/src/java/IslandFurniture/WAR3/MOBILEAPP/RESTAPIFILTER.java
+++ IslandFurniture/IslandFurniture-webservices-war/src/java/IslandFurniture/WAR3/MOBILEAPP/RESTAPIFILTER.java
@@ -15,7 +15,6 @@ public class RESTAPIFILTER implements ContainerResponseFilter,ContainerRequestFi
     private final static Logger log = Logger.getLogger( RESTAPIFILTER.class.getName() );
     @Override
     public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
-        log.info( "Filtering REST Response" );
         responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );    // You may further limit certain client IPs with Access-Control-Allow-Origin instead of '*'
         responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
         responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
