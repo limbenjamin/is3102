768785efa2a6806035f0c1d2f10277ad4a0586f4
Chen Tong <chentong@nus.edu.sg>
2014-11-11 05:54:47 +0800

    Merging

--- IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/ACRM/basketAnalysisBeanLocal.java
+++ IslandFurniture/IslandFurniture-ejb/src/java/IslandFurniture/EJB/ACRM/basketAnalysisBeanLocal.java
@@ -17,8 +17,8 @@ import javax.ejb.Local;
 @Local
 public interface basketAnalysisBeanLocal {
 
-    public List<FurnitureModel> findListOfrelated(FurnitureModel fm, CountryOffice co);
+    List<FurnitureModel> findListOfrelated(FurnitureModel fm, CountryOffice co);
 
-    public List<FurnitureModel> findListOfrelated(String fm, String co);
+    List<FurnitureModel> findListOfrelated(String fm, String co);
     
 }
